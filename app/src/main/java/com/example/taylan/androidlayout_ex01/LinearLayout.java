a48599a40c6b3f1301e25bb943adc28aa963894e
Taylan Uregen <Taylan Uregen>
2016-10-14 09:12:05 +0200

    Test

--- app/src/main/java/com/example/taylan/androidlayout_ex01/LinearLayout.java
+++ app/src/main/java/com/example/taylan/androidlayout_ex01/LinearLayout.java
@@ -9,5 +9,6 @@ public class LinearLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+        //test
     }
 }
